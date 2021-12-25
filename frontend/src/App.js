224569fe5e5fe0646c4d4b16bdc785efe36cbddf
abisalde <abisalde>
2021-12-25 16:17:03 -0500

    CATEGORIES, ActionTypes..........

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,47 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadAllCategories } from './redux/actions/category';
-import { loadAllPosts } from './redux/actions/posts';
-import { LoadingBar } from 'react-redux-loading';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {loadAllCategories} from './redux/actions/category';
+import {loadAllPostsCategory} from './redux/actions/posts';
+import {LoadingBar} from 'react-redux-loading';
 
-const App = () => {
-    const dispatch = useDispatch();
+const App = (...props) => {
+  const dispatch = useDispatch();
 
-    const { categories } = useSelector(({ categories }) => categories);
-    const posts = useSelector(({ posts }) => posts);
-    const loading = useSelector(({ loading }) => loading);
-    const sortBy = useSelector(({ sorted }) => sorted);
+  const categories = useSelector(({categories}) => categories);
+  const posts = useSelector(({posts}) => posts);
+  const loading = useSelector(({loading}) => loading);
+  const sortBy = useSelector(({sorted}) => sorted);
 
-    useEffect(() => {
-        dispatch(loadAllCategories());
-        dispatch(loadAllPosts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(loadAllCategories());
+  }, [dispatch]);
 
-    if (loading.default === undefined || loading.default === 1) {
-        return (
-            <div>
-                <p>App is Loading</p>
-            </div>
-        );
-    }
-
-    const handlePosts = () => {
-        let allPosts = [];
-
-        if (categories && categories.length > 0 && posts) {
-            categories.forEach((category) => {
-                console.log(category.name);
-            });
-        }
-    };
-
-    console.log('testMe', categories);
-
-    console.log('POSTS', handlePosts());
-
+  if (loading.default === undefined || loading.default === 1) {
     return (
-        <div className='App'>
-            <LoadingBar />
-            <div>
-                <h1>Hello, world!</h1>
-                <p>Make Shift</p>
-                <p>{JSON.stringify(categories)}</p>
-                <p>{JSON.stringify(posts)}</p>
-
-                {/* <p>{JSON.stringify(allPosts)}</p> */}
-            </div>
-        </div>
+      <div>
+        <p>App is Loading</p>
+      </div>
     );
+  }
+
+  // if (categories && categories.length > 0) {
+  //   categories.forEach((category) => {
+  //     dispatch(loadAllPostsCategory(category));
+  //   });
+  // }
+
+  return (
+    <div className='App'>
+      <LoadingBar />
+      <div>
+        <h1>Hello, world!</h1>
+        <p>Make Shift</p>
+        <p>{JSON.stringify(categories)}</p>
+        <p>{JSON.stringify(posts)}</p>
+        {/* <p>{JSON.stringify(allPosts)}</p> */}
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/redux/reducers/posts.js
+++ frontend/src/redux/reducers/posts.js
@@ -1,11 +1,14 @@
-import { GET_ALL_POSTS } from '../actions/types';
+import { GET_ALL_POSTS, GET_CATEGORY_POSTS_SUCCESS } from '../actions/types';
 
 const PostsReducer = (state = {}, action) => {
     switch (action.type) {
         case GET_ALL_POSTS:
+            return action.payload;
+        case GET_CATEGORY_POSTS_SUCCESS:
+            const { category, posts } = action.payload;
             return {
                 ...state,
-                ...action.payload,
+                [category]: posts,
             };
         default:
             return state;
