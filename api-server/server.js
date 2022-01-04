0cd7cf58e53208fc1cf24585afbb843047a1cf84
Isaiah Abiodun <38655623+abisalde@users.noreply.github.com>
2022-01-04 16:55:20 -0500

    Update Server.js

--- api-server/server.js
+++ api-server/server.js
@@ -306,10 +306,11 @@ app.listen(config.port, () => {
 });
 
 const path = require('path');
+app.use('/api/v1', require(path.join(__dirname, 'posts', 'comments', 'categories'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
   });
 }
